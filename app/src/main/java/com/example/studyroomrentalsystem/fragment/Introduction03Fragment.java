eebed6cb1a68aae548b7aab8cfc25a0510dbd307
HiepQuoc <hiepquoc0607@gmail.com>
2023-10-27 14:16:15 +0700

    Login And Register Page

---
+++ app/src/main/java/com/example/studyroomrentalsystem/fragment/Introduction03Fragment.java
@@ -0,0 +1,66 @@
+package com.example.studyroomrentalsystem.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.studyroomrentalsystem.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Introduction03Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Introduction03Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Introduction03Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Introduction03Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Introduction03Fragment newInstance(String param1, String param2) {
+        Introduction03Fragment fragment = new Introduction03Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction03, container, false);
+    }
+}
